c804413cf10d59b0d8547fca4f2b236a2414173f
james.meyer <james@marla-XPS-15-7590.lan>
2021-11-01 18:00:56 -0500

    created source code directory hierarchy

---
+++ src/build/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+# build.sh
+#	Compiles the API and transpiles the UI.
---
+++ src/build/release.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+# release.sh
+# 	Generates a tarball, which is used to move the deployment files.
