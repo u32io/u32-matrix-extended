3bd0ed4102c59e89ffd8e50bfef081e5529ffb6f
James Meyer <jamesjmeyer210@gmail.com>
2022-02-04 13:02:16 -0600

    added model lib and user model

---
+++ src/api/u32-matrix-api/matrix-web-model/src/v1/user.rs
@@ -0,0 +1,25 @@
+use matrix_http_client::model::RegisterRequest;
+use matrix_web_dto::v1::user::RegisterUserDTO;
+
+pub struct User {
+    username: String,
+    password: Option<String>,
+}
+
+impl From<RegisterUserDTO> for User {
+    fn from(src: RegisterUserDTO) -> Self {
+        Self {
+            username: src.user_name,
+            password: Some(src.password),
+        }
+    }
+}
+
+impl From<RegisterRequest> for User {
+    fn from(src: RegisterRequest) -> Self {
+        Self {
+            username: src.username,
+            password: None,
+        }
+    }
+}
